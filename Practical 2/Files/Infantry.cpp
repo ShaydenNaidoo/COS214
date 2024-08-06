375771231739c41cdf4327adf4cd91fa23cce77f
Shayden Naidoo <140940045+ShaydenNaidoo@users.noreply.github.com>
2024-08-06 19:03:21 +0200

    added copy constructor for Clone method

--- Practical 2/Files/Infantry.cpp
+++ Practical 2/Files/Infantry.cpp
@@ -2,6 +2,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
+Infantry::Infantry(){
+    this->healthPerSoldier = 0;
+    this->damagePerSoldier = 0;
+    this->defencePerSoldier = 0;
+    this->unitName = "";
+    this->amountOfSoldiersPerUnit = 0;
+}
+Infantry::Infantry(const Infantry &infantry){
+    this->healthPerSoldier = infantry.healthPerSoldier;
+    this->damagePerSoldier = infantry.damagePerSoldier;
+    this->defencePerSoldier = infantry.defencePerSoldier;
+    this->unitName = infantry.unitName;
+    this->amountOfSoldiersPerUnit = infantry.amountOfSoldiersPerUnit;
+}
 Infantry::Infantry(int healthPerSoldier, int damagePerSoldier, int defencePerSoldier, int amountOfSoldiersPerUnit, string unitName):Soldiers(amountOfSoldiersPerUnit){
     this->healthPerSoldier = healthPerSoldier;
     this->damagePerSoldier = damagePerSoldier;
--- Practical 2/Files/Infantry.h
+++ Practical 2/Files/Infantry.h
@@ -14,6 +14,7 @@ class Infantry: public Soldiers{
 
     public:
         Infantry();
+        Infantry( Infantry* infantry);// added copy constructor -Shayden for Clone method
         Infantry(int healthPerSoldier, int damagePerSoldier, int defencePerSoldier, int amountOfSoldiersPerUnit, std::string unitName);
         ~Infantry();
         Soldiers* clonis();
