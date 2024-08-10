630dfccbbc8bd671c99f0b1260006d38e4829a25
Shayden Naidoo <140940045+ShaydenNaidoo@users.noreply.github.com>
2024-08-10 21:49:10 +0200

    added Soldier setters and currnum soldiers

--- Practical 2/Files/Soldiers.h
+++ Practical 2/Files/Soldiers.h
@@ -6,21 +6,29 @@
 #include "Memento.h"
 
 class Soldiers { 
-    private:
+  private:
         int healthPerSoldier;
         int damagePerSoldier;
         int defencePerSoldier;
         char type;
+        int currSold;
         std::string unitName;
 
     public:
         Soldiers();
-        Soldiers(int health, int damage, int defence, std::string name, char type);        
+        Soldiers(int health, int damage, int defence, std::string name, char type,int currSold);        
         int getHealthPerSoldier() const;
         int getDamagePerSoldier() const;
         int getDefencePerSoldier()const;
         std::string getUnitName() const;
         char getType() const;
+        void setCurrNumSoldiers(int num);
+        //tHESE SETTERS ARE FOR THE MEMENTO TESTING
+        void setHealthPerSoldier(int health);
+        void setDamagePerSoldier(int damage);
+        void setDefencePerSoldier(int defence);
+        ////////////////////////////////////////
+        int getCurrNumSoldiers() const;
         void engage();
         void disengage();
         Memento* militusMemento();
@@ -30,6 +38,7 @@ class Soldiers {
         virtual void execute() = 0;
         virtual void retreat() =0;
         virtual void rest() = 0;
+
         virtual ~Soldiers(){}
 };
 
